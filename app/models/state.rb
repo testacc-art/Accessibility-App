953117c6fd5fa3b436965604073a72d9c59f3896
Allison <allimacq@gmail.com>
2021-05-17 16:38:10 -0400

    add model associations

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,4 @@
 class State < ApplicationRecord
+    has_many :cities
+    has_many :trails, through: :cities
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,5 @@
 class Trail < ApplicationRecord
+    has_many :cities
+    has_many :states, :through :cities
+    ratyrate_rateable 'accessibility', 'parking', 'environment'
 end
