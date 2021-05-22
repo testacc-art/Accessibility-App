5f5db487838320bab3ed0b783a847a1a6c618b98
Allison <allimacq@gmail.com>
2021-05-21 22:17:59 -0400

    fix typo in trail model & add home#index

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,5 +1,5 @@
 class Trail < ApplicationRecord
     has_many :cities
-    has_many :states, :through :cities
+    has_many :states, through: :cities
     ratyrate_rateable 'accessibility', 'parking', 'environment'
 end
